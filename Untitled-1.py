e0e511e33ae0a49dc07d6368f7ffd76ed8ff3467
hey54 <shapoval567@gmail.com>
2024-12-19 20:42:19 +0300

    v 1.0

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,13 +1,13 @@
 from pygame import *
-from time import *
-w = 700
+from time import time as timer
+w = 600
 h = 500
 window = display.set_mode((w, h))
 display.set_caption('pingpong')
 c = (121,34,76)
 window.fill(c)
 game = True
-
+finish = False
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -24,26 +24,56 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.y += self.speed
-        if keys[K_d] and self.rect.x < w -80:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < w -80:
+            self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.y += self.speed
-        if keys[K_RIGHT] and self.rect.x < w -80:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < w -80:
+            self.rect.y += self.speed
 
 
-rocket = Player('ba.png', 80, -80, 20, 20, 1)
-rocket2 = Player('ba.png', 480, -80, 20, 20, 1)
+rocket = Player('ro.png', 30, 200, 50, 150, 4)
+rocket2 = Player('ro.png', 520,200, 50, 150, 4)
+ball = GameSprite('ba.png', 200, 200, 50, 50, 4)
 
+font.init()
+font = font.Font(None,35)
+lose = font.render(' player 1 lose', True, (180,0,0))
+lose2 = font.render(' player 2 lose', True, (180,0,0))
+
+speed_x = 3
+speed_y = 3
 FPS = 60
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.fill(c)
+    
+    if finish != True:
+        window.fill(c)
+        rocket.update_l()
+        rocket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(rocket,ball) or sprite.collide_rect(rocket2,ball) :
+            speed_x *= -1
+        if ball.rect.y > h - 50 or ball.rect.y < 0 :
+            speed_y *= -1
+        if ball.rect.x < 0:
+            window.blit(lose,(200,200))
+            finish = True
+        if ball.rect.x > w:
+            window.blit(lose2,(200,200))
+            finish = True
+        rocket.reset()
+        rocket2.reset()
+        ball.reset()
+
     display.update()
+    time.delay(20)   
